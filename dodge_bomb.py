f61588a2ae82507cc83fb094326e7088c59cf8c7
Akichika <c0a24204d1@edu.teu.ac.jp>
2025-06-17 18:16:56 +0900

    空白にコメントに関する #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,18 +9,17 @@ DELTA = { #移動量辞書
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
-    pg.K_RIGHT: (+5, 0), 
-    
+    pg.K_RIGHT: (+5, 0),     
 }
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
-    #引数：こうかとんRectまたは爆弾Rect
-    # #戻り値：横方向，縦方向の画面内外判定結果
-    # #画面内ならTrue，画面外ならFalse
-    
-    
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：横方向，縦方向の画面内外判定結果
+    画面内ならTrue，画面外ならFalse
+    """    
     yoko, tate = True, True #初期値：画面内
     if rct.left < 0 or WIDTH < rct.right: #横方向の画面外判定
         yoko = False
@@ -28,53 +27,45 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate #横方向,　縦方向の画面内判定結果を返す
 
-
-
-
-
-
     
 def gameover(screen: pg.Surface) -> None:
     """ゲームオーバー時に，半透明の黒い画面上に「Game Over」と表示し，泣いているこうかとん画像を貼り付ける関数
        引数 screen: pg.Surface
        戻り値   設定した画像と文字
-    """
-    
+    """ 
     fin_img = pg.Surface((WIDTH,HEIGHT)) #空のsurfceを作る(ブラックアウト)
     pg.draw.rect(fin_img, (0,0,0),pg.Rect(0,0,1600,900)) #四角を線画がする
     fin_img.set_alpha(200) #透明メソッドで半透明にする
     font = pg.font.Font(None, 50) #フォントサイズを設定する
     txt = font.render("Game Over", True, (255, 255, 255)) #ゲームオーバ-と書かれたSurfaceを生成する
-    txt_rct = txt.get_rect()
+    txt_rct = txt.get_rect() #文字Surfaceに対応する文字Rectを取得する
     txt_rct.center = WIDTH/2, HEIGHT/2 #文字の座標を中央に設定する
-    ff_img = pg.image.load("fig/8.png")
+    ff_img = pg.image.load("fig/8.png") #画像を挿入する
     ff1_rct = ff_img.get_rect() #画像Surfaceに対応する画像Rectを取得する
     ff2_rct = ff_img.get_rect() #画像Surfaceに対応する画像Rectを取得する
     ff1_rct.center = WIDTH/1.6, HEIGHT/2 #中心座標から右に移動する
     ff2_rct.center = WIDTH/2.7, HEIGHT/2  #中心座標から左に移動する
     screen.blit(fin_img, [0,0])
     screen.blit(txt, txt_rct) #文字surfaceを画面surfaceに転送する
-    screen.blit(ff_img, ff1_rct) #右に貼り付ける
-    screen.blit(ff_img, ff2_rct) #左に貼り付ける
+    screen.blit(ff_img, ff1_rct) #設定した座標(右)に貼り付ける
+    screen.blit(ff_img, ff2_rct) #設定した座標(左)に貼り付ける
     pg.display.update()
-    time.sleep(5)
+    time.sleep(5)                #5秒間表示させる
+
 
 def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     """
     関数:サイズの異なる爆弾Surfaceを要素としたリストと加速度リストを返す
-    戻り値：加速度のリスト，拡大のリスト
+    戻り値：加速度のリスト，拡大のリストのタプルを返す
     """
-    
-
-    bb_imgs = []
-    bb_accs = [a for a in range(1, 11)]
-
-    for r in range(1, 11):
-        bb_img = pg.Surface((20*r, 20*r))
+    bb_imgs = []                        #空のリスト
+    bb_accs = [a for a in range(1, 11)] #加速度リスト
+    for r in range(1, 11):              #拡大爆弾surfaceのリスト
+        bb_img = pg.Surface((20*r, 20*r)) 
         pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
-        bb_imgs.append(bb_img)
-
+        bb_imgs.append(bb_img) #リストに追加する
     return bb_imgs, bb_accs
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -92,10 +83,7 @@ def main():
     vx = +5
     vy = +5
     clock = pg.time.Clock()
-
-   
     tmr = 0
- 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -105,17 +93,14 @@ def main():
             print("ゲームオーバー")
             gameover(screen)
             return
-        
+          
         screen.blit(bg_img, [0, 0]) 
-
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-
-
         #if key_lst[pg.K_UP]:
             #sum_mv[1] -= 5
         #if k#ey_lst[pg.K_DOWN]:
@@ -128,45 +113,22 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) #移動をなかったことにする
         screen.blit(kk_img, kk_rct)
-        
-
         bb_imgs, bb_accs = init_bb_imgs()  
         avx = vx*bb_accs[min(tmr//500, 9)]
         bb_img = bb_imgs[min(tmr//500, 9)]
-        bb_rct.move_ip(avx, vy) #爆弾の移動
+        bb_rct.move_ip(avx, vy) #爆弾の移動 avxでvxの設定
         bb_img.set_colorkey((0,0,0)) #上書きされたので再度黒を透明色
-
         yoko, tate = check_bound(bb_rct)
         if not yoko: #横方向にはみ出ていたら
             vx *= -1
         if not tate: #縦方向にはみ出ていたら
             vy *= -1
-
-
-        
         screen.blit(bb_img, bb_rct) #爆弾の線画
-        
-
-        
-
         pg.display.update()
         tmr += 1
         clock.tick(50)
     
         
-        
-        
-
-
-
-
-
-
-
-
-    
-
-
 if __name__ == "__main__":
     pg.init()
     main()
